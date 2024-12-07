55a3447a2617775811abe44a704e20f57e908af9
FFengIll <fengyouzheng@gmail.com>
2024-12-07 09:27:09 +0800

    test: update.

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -20,6 +20,7 @@ func TestServiceParsing(t *testing.T) {
 		namespace go somepkg
 		namespace python some.module123
 		namespace python.py-twisted another
+		namespace c_glib lib
 
 		const map<string,string> M1 = {"hello": "world", "goodnight": "moon"}
 		const string S1 = "foo\"\tbar"
